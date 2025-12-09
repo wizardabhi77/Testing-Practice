d9e033b3aa276f125297fb3509ecc78be8516a13
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-12-09 13:39:46 +0530

    test ceasarCipher function

--- tDD.js
+++ tDD.js
@@ -8,3 +8,50 @@ export function reverseString(str) {
     return [...str].reverse().join("");
 }
 
+export const calculator = function () {
+
+    function add(a,b) {
+        return a+b;
+    }
+
+    function subtract(a,b){
+        return a-b;
+    }
+
+    function multiply(a,b){
+        return a*b;
+    }
+
+    function divide(a,b){
+        return a/b;
+    }
+
+    return {add, subtract, multiply, divide};
+}();
+
+export function ceasarCipher(str, key) {
+
+    let cipher = [];
+    
+    for(let i=0; i<str.length; i++){
+        
+        let currCode = str.charCodeAt(i);
+
+        if((currCode > 64 && currCode < 91) || (currCode > 96 && currCode < 123)){
+
+            if((currCode <97 && currCode+key > 90)||(currCode >90 && currCode+key >122)){
+                currCode -= 26;
+            }
+            
+
+            cipher.push(String.fromCharCode(currCode+key));
+        }
+
+        else{
+            cipher.push(str[i]);
+        }
+    }
+   
+    return cipher.join("");
+}
+
--- tDD.test.js
+++ tDD.test.js
@@ -1,5 +1,6 @@
 
-import {capitalize, reverseString} from './tDD';
+import {capitalize, reverseString, calculator, ceasarCipher} from './tDD';
+
 
 
 test('first letter is capitalized', () =>
@@ -14,10 +15,39 @@ test('first letter is capitalized', () =>
 expect(capitalize('CHRIST')).toBe('CHRIST')
 );
 
-test('first letter is capitalized', () =>
+test('String is Reversed', () =>
 expect(reverseString('CHRIST')).toBe('TSIRHC')
 );
 
-test('first letter is capitalized', () =>
+test('String is reversed', () =>
 expect(reverseString('malayalam')).toBe('malayalam')
 );
+
+test('calculate add', () =>
+expect(calculator.add(1,2)).toBe(3));
+
+test('calculate subtract', () =>
+expect(calculator.subtract(1,2)).toBe(-1));
+
+test('calculate multiply', () =>
+expect(calculator.multiply(1,2)).toBe(2));
+
+test('calculate divide', () =>
+expect(calculator.divide(1,0)).toBe(Infinity));
+
+//Ceasar Cipher test
+
+test('ceasarCipher is a function', ()=> 
+expect(ceasarCipher).toBeDefined())
+
+test('cipher of string with no special characters', ()=>
+expect(ceasarCipher('abc',3)).toBe('def'));
+
+test('cipher of string with wrapping', ()=>
+expect(ceasarCipher('xyz',3)).toBe('abc'));
+
+test('cipher of string with case preservation', ()=>
+expect(ceasarCipher('heLLo',3)).toBe('khOOr'));
+
+test('cipher of string with special characters', ()=>
+expect(ceasarCipher('Hello, World!',3)).toBe('Khoor, Zruog!'));
